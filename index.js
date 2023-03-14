db2a57e8330c478750c2c029d121bcb78563127e
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-03-14 16:05:24 +0900

    for in/for of/Symbol

--- index.js
+++ index.js
@@ -1,112 +1,63 @@
-// class
-class 할아버지 {
-  constructor(이름) {
-    this.name = 이름;
-  }
-  sayHi() {
-    console.log(`i'm your grandfather`);
-  }
-}
-class 아버지 extends 할아버지 {
-  constructor(이름, 나이) {
-    super(이름);
-    this.age = 나이;
-  }
-  sayHello() {
-    super.sayHi();
-    console.log(`I'm your father`);
-  }
-}
-const 아버지1 = new 아버지();
-console.log(아버지1.sayHello());
+// for in
 
-// getter & setter
-const hyewon = {
-  name: "hana",
-  age: 27,
-  set nextAge(나이) {
-    this.age = 나이 + 1;
+class 부모 {}
+부모.prototype.name = "kim";
+
+const obj = new 부모();
+
+for (let key in obj) {
+  if (obj.hasOwnProperty(key)) {
+    console.log(obj[key]);
+  }
+}
+
+// for of 구구단
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let 자료 of arr) {
+  for (let 자료2 of arr2) {
+    console.log((자료 + 1) * 자료2);
+  }
+}
+
+// 연습문제
+const products1 = [
+  {
+    name1: "chair",
+    price1: 7000,
   },
+  {
+    name2: "sofa",
+    price: 5000,
+  },
+  {
+    name1: "desk",
+    price3: 9000,
+  },
+];
+// function 제거(products) {
+//   let result = {};
+//   for (let i of products) {
+//     for (let key in i) {
+//       if (typeof key.slice(-1) == "number") {
+
+//       } else {
+//       }
+//     }
+//   }
+// }
+// console.log(제거(products1));
+
+// 심볼
+const 심볼 = Symbol("설명");
+const weight = Symbol("몸무게 비밀");
+const height = Symbol("키 비밀임ㄴ");
+const person = {
+  name: "Lee",
 };
-hyewon.nextAge = 40;
-console.log(hyewon.age);
+person.weight = 45;
 
-// class, extends, getter, setter 연습문제 5개
-//1.
-class Dog {
-  constructor(타입, 색깔) {
-    this.type = 타입;
-    this.color = 색깔;
-  }
-}
-2;
-class Cat extends Dog {
-  constructor(타입, 색깔, 나이) {
-    super(타입, 색깔);
-    this.age = 나이;
-  }
-}
-// 3.
-class Cat extends Dog {
-  constructor(타입, 색깔, 나이) {
-    super(타입, 색깔);
-    this.age = 나이;
-  }
-  set setAge(몇살) {
-    this.age = Number(몇살 + 1);
-  }
-  get nextAge() {
-    return this.age;
-  }
-}
+person[weight] = 200;
 
-const kitty = new Cat(2, 1, 20);
-console.log(kitty);
-const puppy = new Dog(4, 5, 6);
-console.log(puppy);
-kitty.setAge = 29;
-console.log(kitty.nextAge);
-console.log(puppy.nextAge);
-
-4;
-class Unit {
-  constructor() {
-    this.공격력 = 5;
-    this.체력 = 100;
-  }
-  get battlePoint() {
-    return this.공격력 + this.체력;
-  }
-  set heal(증가) {
-    this.체력 += Number(증가);
-  }
-}
-const 캐릭터 = new Unit();
-캐릭터.heal = 70;
-console.log(캐릭터.battlePoint);
-
-//5.
-const data = {
-  odd: [],
-  even: [],
-  분리해주는함수: function (...x) {
-    for (let i = 0; i < x.length; i++) {
-      if (x[i] % 2 == 0) {
-        this.even.push(x[i]);
-      } else {
-        this.odd.push(x[i]);
-      }
-    }
-    return data;
-  },
-  겟함수: function () {
-    const result = [...this.odd, ...this.even].sort(function (comp1, comp2) {
-      return comp1 - comp2;
-    });
-
-    return result;
-  },
-};
-
-console.log(data.분리해주는함수(11, 8, 3, 4));
-console.log(0, data.겟함수());
+console.log(person.weight);
