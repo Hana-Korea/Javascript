c6805075a7a15fee36c91b6741910b8a12981043
Hana-Korea <117172983+Hana-Korea@users.noreply.github.com>
2023-03-16 18:53:23 +0900

    movie website

--- index.js
+++ index.js
@@ -1,24 +1,80 @@
-// const btnEl = document.querySelector("button");
-// async function 버튼클릭함수() {
-//   const 프로미스 = new Promise(function (res, rej) {
-//     btnEl.addEventListener("click", () => {
-//       res("ㅇㅇ");
-//     });
-//   });
-//   프로미스.then((결과) => {
-//     console.log(결과);
-//   });
-// }
-// 버튼클릭함수();
-
+const body = document.querySelector("body");
 const btnEl = document.querySelector("button");
-async function 버튼클릭함수() {
-  const 프로미스 = new Promise(function (res, rej) {
-    btnEl.addEventListener("click", () => {
-      res("ㅇㅇ");
+const inputEl = document.querySelector("input");
+inputEl.addEventListener("input", () => {
+  const title = inputEl.value;
+  clickBtn(title);
+});
+
+function clickBtn(title) {
+  new Promise(function (res, rej) {
+    btnEl.addEventListener("click", (e) => {
+      e.preventDefault();
+      const title = inputEl.value;
+      inputEl.value = "";
+      getMovies(title).then((movie) => {
+        createMovieList(movie);
+      });
+    });
+    res();
+  });
+}
+
+function createMovieList(movie) {
+  const movieList = document.createElement("div");
+  movie.forEach((item) => {
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movieContainer");
+    const movieTitle = document.createElement("div");
+    const movieImg = document.createElement("img");
+    const movieYear = document.createElement("span");
+    const movieId = document.createElement("div");
+    const moviePlot = document.createElement("div");
+    movieId.textContent = item.imdbID;
+    movieTitle.textContent = item.Title;
+    movieYear.textContent = item.Year;
+    movieImg.src = item.Poster;
+    movieContainer.append(movieYear);
+    movieContainer.append(movieImg);
+    movieContainer.append(movieTitle);
+    movieList.append(movieContainer);
+    movieContainer.addEventListener("click", () => {
+      const realMovieId = movieId.textContent;
+      getMovieDetail(realMovieId).then((plot) => {
+        moviePlot.textContent = plot;
+        movieContainer.append(moviePlot);
+      });
     });
   });
-  const result = await 프로미스;
-  console.log(result);
+  body.appendChild(movieList);
 }
-버튼클릭함수();
+
+async function getMovies(title) {
+  const res = await fetch(
+    `https://omdbapi.com/?apikey=7035c60c${`&s=${title}`}`
+  )
+    .then((result) => {
+      const json = result.json();
+      return json;
+    })
+    .then((json) => {
+      const movie = json.Search;
+      return movie;
+    });
+  return res;
+}
+
+async function getMovieDetail(id) {
+  const res = await fetch(
+    `https://omdbapi.com/?apikey=7035c60c&i=${id}&plot=full`
+  )
+    .then((result) => {
+      const json = result.json();
+      return json;
+    })
+    .then((json) => {
+      const plot = json.Plot;
+      return plot;
+    });
+  return res;
+}
